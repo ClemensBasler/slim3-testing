67d15b6e1bc9bd93c7a6d695bc7fe8abcf60a123
Clemens Basler <clemens-basler@t-online.de>
2018-03-19 13:44:59 +0100

    add

---
+++ server.php
@@ -0,0 +1,28 @@
+<?php
+require 'vendor/autoload.php';
+// Create and configure Slim app
+$config = ['settings' => [
+    'addContentLengthHeader' => false,
+]];
+$app = new \Slim\App($config);
+
+// Define app routes
+
+// $newResponse = withJson($data);
+
+$app->get('/json', function ($request, $response, $args) {
+  // $response2 = $response->withHeader('Content-type', 'application/json');
+  // $data = array('name' => 'Bob', 'age' => 40);
+
+  $data = array('name' => 'Bob', 'age' => 40);
+  $response->withHeader('Content-Type', 'application/json');
+  $response->write(json_encode($data));
+  return $response;
+});
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->write("Hello " . $args['name']);
+});
+
+// Run app
+$app->run();
